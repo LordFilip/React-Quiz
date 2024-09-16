d65534e13feeff483ecf9bef4eef5aea4afb4f60
Filip <filipivanovic59@gmail.com>
2024-09-16 21:00:29 +0200

    Theme Changing

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,20 +1,36 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { QuizContext } from "./App";
 import Difficulty from "./Difficulty";
 
 export default function StartScreen() {
   const { numQuestions, dispatch } = useContext(QuizContext);
 
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkTheme((prevTheme) => !prevTheme);
+    if (!isDarkTheme) {
+      document.documentElement.classList.add("dark-theme");
+    } else {
+      document.documentElement.classList.remove("dark-theme");
+    }
+  };
+
   return (
     <div className="start">
       <h2>Welcome to the React Quiz</h2>
       <h3>{numQuestions} questions to test your React mastery</h3>
       <Difficulty />
+
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: "start" })}
       >
         Let's start
+      </button>
+
+      <button className="btn btn-ui btn-switch" onClick={toggleTheme}>
+        {isDarkTheme ? "Switch to Light Theme" : "Switch to Dark Theme"}
       </button>
     </div>
   );
